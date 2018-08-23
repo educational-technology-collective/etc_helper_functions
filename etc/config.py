e9e3ae07d1bc7f269926e5e3411d35d7aba70325
Anant Mittal <mittal.anant@gmail.com>
2018-08-24 03:16:39 +0530

    Removed few important files

--- etc/config.py
+++
@@ -1,8 +0,0 @@
-class Config:
-    '''
-        base configuration class
-    '''
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://proxy_ma_system:gZAMAyf2SEn96d@etc-db.cmb61qv9ob3s.us-east-1.rds.amazonaws.com/proxy_ma'
